710c9ced548b0d5efebf8ac2006c028e0fd77626
Nick <65061325+psw0962@users.noreply.github.com>
2021-12-09 13:18:23 +0900

    개발환경 셋팅

---
+++ src/pages/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+import { TMDB_KEY } from '../config';
+
+
+const Home = () => { 
+    const testReq = async() => {
+        const req = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${TMDB_KEY}&language=en-US&page=1&include_adult=false&query=harry potter`);
+        console.log(req.data)
+    }
+
+    testReq();
+    return (
+        <div>asdasdas</div>
+    )
+}
+
+export default Home;
