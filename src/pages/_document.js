a4a41e86f4e4d71508f50c2fe71968701b409e61
Nick <65061325+psw0962@users.noreply.github.com>
2022-01-02 14:28:09 +0900

    추가 : 검색결과 영화상세페이지 데이터 유지

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -22,7 +22,7 @@ export default class MyDocument extends Document {
         ),
       };
     } catch (err) {
-      console.log(err);
+      console.error(err);
     } finally {
       sheet.seal();
     }
--- src/pages/moviedetail/[id].js
+++ src/pages/moviedetail/[id].js
@@ -7,9 +7,7 @@ import * as Font from '../../components/common/Font';
 import { TMDB_KEY } from '../../config';
 
 const MovieDetail = () => {
-  const [detailData, setDetailData] = useState(
-    () => JSON.parse(JSON.stringify(window.localStorage.getItem('detailData'))) || null
-  );
+  const [detailData, setDetailData] = useState(null);
   const router = useRouter();
   const { id } = router.query;
   const test = `https://api.themoviedb.org/3/movie/${id}?api_key=${TMDB_KEY}&language=ko`;
@@ -25,21 +23,25 @@ const MovieDetail = () => {
 
   useEffect(() => {
     getDetailMovieDataAPI();
+
+    if (typeof window !== undefined) {
+      const storedValue = localStorage.getItem('detailData');
+      console.log(storedValue);
+      setDetailData(JSON.parse(storedValue));
+    }
   }, []);
 
   useEffect(() => {
     window.localStorage.setItem('detailData', JSON.stringify(detailData));
   }, [detailData]);
-  console.log('asdasdasdas', detailData);
+
   return (
     <AppLayout>
-      {detailData !== null ? (
+      {detailData && (
         <Wrapper>
           <img src={`https://image.tmdb.org/t/p/w200${detailData.poster_path}`} />
           <FontSize22>{detailData.title}</FontSize22>
         </Wrapper>
-      ) : (
-        <div>검색결과가 없습니다.</div>
       )}
     </AppLayout>
   );
